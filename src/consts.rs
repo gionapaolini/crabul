4e9f67e3e1d68c37a193a54e3b5998129cbe1bec
Giona Paolini <gionapaolini@gmail.com>
2024-11-16 17:30:58 +0100

    change times

--- src/consts.rs
+++ src/consts.rs
@@ -15,7 +15,7 @@ pub enum GameError {
 
 pub const MIN_PLAYERS: usize = 2;
 pub const MAX_PLAYERS: usize = 6;
-pub const PEEKING_PHASE_COUNTDOWN: Duration = Duration::from_secs(1);
+pub const PEEKING_PHASE_COUNTDOWN: Duration = Duration::from_secs(10);
 pub const TURN_COUNTDOWN: Duration = Duration::from_secs(600);
 
 pub type RoomId = u16;
